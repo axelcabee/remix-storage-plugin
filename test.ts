473aeb05030c037422a40676be2ab8c311840b9e
filip mertens <filip.mertens@ethereum.org>
2021-09-06 16:50:01 +0200

    rm test

--- test.ts
+++
@@ -1,9 +0,0 @@
-// this slow call will be canceled after 1 second, even if the timeout of someplugin is 10secs.
-try {
-    setTimeout(() => {
-        client.cancel('someplugin', 'slowcall')
-    }, 1000)
-    await client.call('someplugin', 'slowcall', data);
-} catch (e) {
-    console.log(e)
-}
