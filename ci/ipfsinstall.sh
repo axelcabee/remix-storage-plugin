7ba2838c2c72d6706cf6f593b0d880487a31285a
filip mertens <filip.mertens@ethereum.org>
2021-10-11 16:43:31 +0200

    no check

--- ci/ipfsinstall.sh
+++ ci/ipfsinstall.sh
@@ -1,5 +1,4 @@
-apt-get install ca-certificates
-wget https://dist.ipfs.io/go-ipfs/v0.9.1/go-ipfs_v0.9.1_linux-amd64.tar.gz
+wget --no-check-certificate https://dist.ipfs.io/go-ipfs/v0.9.1/go-ipfs_v0.9.1_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.9.1_linux-amd64.tar.gz
 cd go-ipfs
 sudo bash install.sh
